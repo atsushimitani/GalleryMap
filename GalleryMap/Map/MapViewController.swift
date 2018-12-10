4196818ebe9ff55ab04142e984a72bd4f0af93bd
Atsushi Mitani <tech.mitani@gmail.com>
2018-12-10 21:11:29 +0900

    リファクタリング中

--- GalleryMap/Map/MapViewController.swift
+++ GalleryMap/Map/MapViewController.swift
@@ -15,33 +15,29 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
     
     @IBOutlet weak var mapView: GMSMapView!
 
+    // MapViewに移したい
     private var locationManager: CLLocationManager?
     private var currentLocation: CLLocation?
     private var placesClient: GMSPlacesClient!
     private var zoom: Float = 15.0
 
-    private var initView: Bool = false
+    private var needInitView = false
+    
+    private let needLoadFromFirestore = false
     
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        self.mapView.isMyLocationEnabled = true
-        self.mapView.mapType = GMSMapViewType.normal
-        self.mapView.settings.compassButton = true
-        self.mapView.settings.myLocationButton = true
-        self.mapView.delegate = self
+        setup()
 
-        // 位置情報関連の初期化
-        self.locationManager = CLLocationManager()
-        self.locationManager?.desiredAccuracy = kCLLocationAccuracyBest
-        self.locationManager?.requestAlwaysAuthorization()
-        self.locationManager?.distanceFilter = 50
-        self.locationManager?.startUpdatingLocation()
-        self.locationManager?.delegate = self
-        self.placesClient = GMSPlacesClient.shared()
+        // ここから下はMapクラスに委譲できる
+        initLocationManager()
+        
         
         // TODO 位置情報を取得できない場合の対応
         
+        
+        // loadmapinfoメソッドに切り出して、オリジナルクラスに移す
         // Firestoreからデータ取得
         let db = Firestore.firestore()
         db.collection("galleries").getDocuments() { (querySnapshot, err) in
@@ -52,7 +48,9 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
                 for document in querySnapshot!.documents {
                     print("\(document.documentID) => \(document.data())")
                     
-                    var gallery = document.data()
+                    let gallery = document.data()
+                    
+                    // TODO データをオブジェクトに格納
 
                     // マーカーを描画
                     let marker = GMSMarker()
@@ -63,7 +61,7 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
                     var snipet = ""
                     if let genres = gallery["genre"] as? [String] {
                         for genre in genres {
-                            snipet += "\(genre) "   
+                            snipet += "\(genre) "
                         }
                     }
                     marker.snippet = snipet
@@ -71,31 +69,51 @@ class MapViewController: UIViewController, GMSMapViewDelegate, CLLocationManager
                 }
             }
         }
-   }
+        
+        // TODO: - Realm
+        if (needLoadFromFirestore) {
+//            loadMapInfo()
+        } else {
+//            loadMapInfo()
+        }
+    }
+    
+    private func setup() {
+        mapView.isMyLocationEnabled = true
+        mapView.mapType = GMSMapViewType.normal
+        mapView.settings.compassButton = true
+        mapView.settings.myLocationButton = true
+        mapView.delegate = self
+    }
+    
+    // MARK: - Google Map Functions
+    
+    private func initLocationManager() {
+        // 位置情報関連の初期化
+        // MapViewに入れてもいいと思う
+        locationManager = CLLocationManager()
+        locationManager?.desiredAccuracy = kCLLocationAccuracyBest
+        locationManager?.requestAlwaysAuthorization()
+        locationManager?.distanceFilter = 50
+        locationManager?.startUpdatingLocation()
+        locationManager?.delegate = self
+        placesClient = GMSPlacesClient.shared()
+    }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
     
+    // オリジナルクラスに移す
     func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
-        if !self.initView {
+        if !needInitView {
             // 初期描画時のマップ中心位置の移動
-            let camera = GMSCameraPosition.camera(withTarget: (locations.last?.coordinate)!, zoom: self.zoom)
-            self.mapView.camera = camera
-            self.locationManager?.stopUpdatingLocation()
-            self.initView = true
+            let camera = GMSCameraPosition.camera(withTarget: (locations.last?.coordinate)!, zoom: zoom)
+            mapView.camera = camera
+            locationManager?.stopUpdatingLocation()
+            needInitView = true
         }
     }
-    
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destinationViewController.
-        // Pass the selected object to the new view controller.
-    }
-    */
 
 }
